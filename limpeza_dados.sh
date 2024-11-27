7ab8dc8fe53c7f86a036667979cb828386e2eb27
romano-kind <felipe.lromano@gmail.com>
2024-11-27 11:38:56 -0300

    Adiciona README e scripts de limpeza e alinhamento. Loop 'for' incluido no script de limpea

--- limpeza_dados.sh
+++ limpeza_dados.sh
@@ -1,9 +1,14 @@
 # Avaliação de qualidade das leituras com FastQC
-# nesta linha, será gerado um arquivo fastqc (relatório de qualidade) a partir de um arquivo fastq
-fastqc SRRXXXXXXX.fastq
+# nesta linha, o script irá gerar um arquivo fastqc (relatório de qualidade) para cada arquivo fastq
+
+for i in *.fasta
+do
+fastqc *.fastq
 
 # Limpeza e corte de sequências de baixa qualidade com Trim Galore
 # nesta linha de comando, foi determinado que será utilizado um corte mínimo de qualidade (Phred 25), os Ns serão removidos da
 #   leitura, será utilizado 1 core de processamento para corte, serão descartadas reads menores que 18 e os arquivos 
-#   de output não serão compactados
-trim_galore --fastqc -q 25 --trim-n --max_n 0 -j 1 --length 18 --dont_gzip SRRXXXXXXX.fastq
+#   de output não serão compactados. O corte será feito para todos os arquivos fastq presentes no diretório.
+
+trim_galore --fastqc -q 25 --trim-n --max_n 0 -j 1 --length 18 --dont_gzip "$file".fastq
+done
